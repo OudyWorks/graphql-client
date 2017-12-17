e517ae758e6d09e244f91183293132ac397323ec
Ayoub Oudmane <ayoub@oudmane.me>
2017-12-17 19:27:33 +0000

    ! Initial Commit

---
+++ build/index.js
@@ -0,0 +1,64 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _apolloClient = require('apollo-client');
+
+var _apolloLinkHttp = require('apollo-link-http');
+
+var _nodeFetch = require('node-fetch');
+
+var _nodeFetch2 = _interopRequireDefault(_nodeFetch);
+
+var _apolloCacheInmemory = require('apollo-cache-inmemory');
+
+var _subscriptionsTransportWs = require('subscriptions-transport-ws');
+
+var _apolloLink = require('apollo-link');
+
+var _apolloLinkWs = require('apollo-link-ws');
+
+var _apolloUtilities = require('apollo-utilities');
+
+var _ws = require('ws');
+
+var _ws2 = _interopRequireDefault(_ws);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// const uri = '10.0.0.21:83'
+
+let client = {
+    configure(uri, options = {
+        query: {
+            fetchPolicy: 'network-only'
+        }
+    }) {
+
+        const httpLink = new _apolloLinkHttp.HttpLink({
+            uri,
+            fetch: _nodeFetch2.default
+        }),
+              wsLink = new _apolloLinkWs.WebSocketLink(new _subscriptionsTransportWs.SubscriptionClient(uri.replace(/^http/, 'ws'), {
+            reconnect: true,
+            // wasKeepAliveReceived: true,
+            timeout: 60000
+        }, _ws2.default)),
+              link = (0, _apolloLink.split)(({ query }) => {
+            const { kind, operation } = (0, _apolloUtilities.getMainDefinition)(query);
+            return kind === 'OperationDefinition' && operation === 'subscription';
+        }, wsLink, httpLink);
+
+        Object.assign(client, new _apolloClient.ApolloClient({
+            link,
+            cache: new _apolloCacheInmemory.InMemoryCache({
+                addTypename: false
+            }),
+            defaultOptions: options
+        }));
+    }
+};
+
+exports.default = client;
---
+++ source/index.js
@@ -0,0 +1,61 @@
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import fetch from 'node-fetch'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { SubscriptionClient } from 'subscriptions-transport-ws'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+import WebSocket from 'ws'
+
+// const uri = '10.0.0.21:83'
+
+let client = {
+    configure(
+        uri,
+        options = {
+            query: {
+                fetchPolicy: 'network-only'
+            }
+        }
+    ) {
+
+        const httpLink = new HttpLink({
+                uri,
+                fetch
+            }),
+            wsLink = new WebSocketLink(
+                new SubscriptionClient(
+                    uri.replace(/^http/, 'ws'),
+                    {
+                        reconnect: true,
+                        // wasKeepAliveReceived: true,
+                        timeout: 60000
+                    },
+                    WebSocket
+                )
+            ),
+            link = split(
+                ({ query }) => {
+                    const { kind, operation } = getMainDefinition(query)
+                    return kind === 'OperationDefinition' && operation === 'subscription'
+                },
+                wsLink,
+                httpLink
+            )
+
+        Object.assign(
+            client,
+            new ApolloClient({
+                link,
+                cache: new InMemoryCache({
+                    addTypename: false
+                }),
+                defaultOptions: options
+            })
+        )
+
+    }
+}
+
+export default client
