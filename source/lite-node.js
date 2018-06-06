208465b60ad8ae64e2187de045247fa1830c7bcf
Ayoub Oudmane <ayoub@oudmane.me>
2018-06-06 13:23:49 +0000

    + lite client

---
+++ source/lite-node.js
@@ -0,0 +1,19 @@
+import liteClient from './index'
+import fetch from 'node-fetch'
+
+let liteNodeClient = Object.assign({}, liteClient)
+
+liteNodeClient.configure = function(
+        uri,
+        {authorization = ''}
+    ) {
+        return liteClient.configure.bind(this)(
+            uri,
+            {
+                fetch,
+                authorization
+            }
+        )
+    }
+
+export default liteNodeClient
---
+++ source/lite.js
@@ -0,0 +1,41 @@
+export default client = {
+    uri: null,
+    fetch: null,
+    authorization: undefined,
+    configure(
+        uri,
+        options = {
+            fetch,
+            authorization: ''
+        }
+    ) {
+        this.uri = options.uri
+        this.fetch = options.fetch || window.fetch
+        this.authorization = options.authorization
+    },
+    request(query, variables = {}) {
+        return this.fetch(
+            this.uri,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': this.authorization
+                },
+                body: JSON.stringify({
+                    query, variables
+                })
+            }
+        ).then(
+            response =>
+                response.json()
+        )
+    },
+    query({query, variables}) {
+        return this.request(query, variables)
+    },
+    mutate({mutation, variables}) {
+        return this.request(mutation, variables)
+    }
+}
