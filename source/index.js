1c894317f956c48054426084fd5316efb516549d
Ayoub Oudmane <ayoub@oudmane.me>
2018-10-09 10:57:34 +0100

    #

--- source/index.js
+++ source/index.js
@@ -31,104 +31,104 @@ let client = {
         let W = options.WebSocket || window.WebSocket
 
         return new Promise(
-            (resolve, reject) =>
-            F(
-                uri,
-                {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': options.authorization
-                    },
-                    body: JSON.stringify({
-                        query: `
-                        {
-                            __schema {
-                              types {
-                                kind
-                                name
-                                possibleTypes {
+            (resolve, reject) => {
+                F(
+                    uri,
+                    {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': options.authorization
+                        },
+                        body: JSON.stringify({
+                            query: `
+                            {
+                                __schema {
+                                types {
+                                    kind
                                     name
+                                    possibleTypes {
+                                        name
+                                    }
                                 }
-                              }
-                            }
-                        }                          
-                        `
-                    })
-                }
-            ).then(
-                result =>
-                    result.json()
-            ).then(
-                result => {
-                
-                    result.data.__schema.types = result.data.__schema.types.filter(
-                        type => type.possibleTypes !== null
-                    )
-    
-                    const httpLink = new (options.batch ? BatchHttpLink : HttpLink)(Object.assign(
-                            {
-                                uri,
-                                fetch: F
-                            },
-                            options.batch || {}
-                        )),
-                        subscriptionClient = new SubscriptionClient(
-                            uri.replace(/^http/, 'ws'),
-                            {
-                                reconnect: true,
-                                // wasKeepAliveReceived: true,
-                                timeout: 60000
-                            },
-                            W
-                        ),
-                        wsLink = new WebSocketLink(
-                            subscriptionClient
-                        ),
-                        authLink = setContext((_, { headers = {} }) => ({
-                            headers: Object.assign(
-                                headers,
-                                {
-                                    authorization: options.authorization
                                 }
-                            )
-                        })),
-                        link = split(
-                            ({ query }) => {
-                                const { kind, operation } = getMainDefinition(query)
-                                return kind === 'OperationDefinition' && operation === 'subscription'
-                            },
-                            wsLink,
-                            httpLink
-                        ),
-                        apolloClient = new ApolloClient({
-                            link: authLink.concat(link),
-                            cache: new InMemoryCache({
-                                addTypename: false,
-                                fragmentMatcher: new IntrospectionFragmentMatcher({
-                                    introspectionQueryResultData: result.data
-                                })
-                            }),
-                            defaultOptions: options.defaultOptions
+                            }                          
+                            `
                         })
-    
-                    Object.assign(
-                        this,
-                        apolloClient
-                    )
-    
-                    this.__proto__ = apolloClient.__proto__
-    
-                    this.subscriptionClient = subscriptionClient
+                    }
+                ).then(
+                    result =>
+                        result.json()
+                ).then(
+                    result => {
+                    
+                        result.data.__schema.types = result.data.__schema.types.filter(
+                            type => type.possibleTypes !== null
+                        )
+        
+                        const httpLink = new (options.batch ? BatchHttpLink : HttpLink)(Object.assign(
+                                {
+                                    uri,
+                                    fetch: F
+                                },
+                                options.batch || {}
+                            )),
+                            subscriptionClient = new SubscriptionClient(
+                                uri.replace(/^http/, 'ws'),
+                                {
+                                    reconnect: true,
+                                    // wasKeepAliveReceived: true,
+                                    timeout: 60000
+                                },
+                                W
+                            ),
+                            wsLink = new WebSocketLink(
+                                subscriptionClient
+                            ),
+                            authLink = setContext((_, { headers = {} }) => ({
+                                headers: Object.assign(
+                                    headers,
+                                    {
+                                        authorization: options.authorization
+                                    }
+                                )
+                            })),
+                            link = split(
+                                ({ query }) => {
+                                    const { kind, operation } = getMainDefinition(query)
+                                    return kind === 'OperationDefinition' && operation === 'subscription'
+                                },
+                                wsLink,
+                                httpLink
+                            ),
+                            apolloClient = new ApolloClient({
+                                link: authLink.concat(link),
+                                cache: new InMemoryCache({
+                                    addTypename: false,
+                                    fragmentMatcher: new IntrospectionFragmentMatcher({
+                                        introspectionQueryResultData: result.data
+                                    })
+                                }),
+                                defaultOptions: options.defaultOptions
+                            })
+        
+                        Object.assign(
+                            this,
+                            apolloClient
+                        )
+        
+                        this.__proto__ = apolloClient.__proto__
+        
+                        this.subscriptionClient = subscriptionClient
 
-                    // temp solution to disable subscription cache
-                    this.store.markSubscriptionResult = function() {}
+                        // temp solution to disable subscription cache
+                        this.store.markSubscriptionResult = function() {}
 
-                    resolve()
-    
-                }
-            ).catch(reject)
-
+                        resolve()
+        
+                    }
+                ).catch(reject)
+            }
         )
 
     }
